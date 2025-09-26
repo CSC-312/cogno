839132fa99f625b1a94ba62d84441aacc130b4be
kevinnkansah <kevinnkansah6@gmail.com>
2025-09-26 18:37:30 +0200

    feat: added audio transcription

--- frontend/main.py
+++ frontend/main.py
@@ -1,13 +1,21 @@
 import os
+import io
+import wave
 from datetime import datetime
 from typing import Any, Dict, Optional
 
+import numpy as np
 import chainlit as cl
 from ollama import AsyncClient
+from openai import AsyncOpenAI
 
 ollama = AsyncClient(
     host=os.getenv("OLLAMA_BASE_URL"),
     headers={"Authorization": f"Bearer {os.getenv('OLLAMA_API_KEY')}"},
+)
+
+whisper = AsyncOpenAI(
+    base_url=os.getenv("GROQ_BASE_URL"), api_key=os.getenv("GROQ_API_KEY")
 )
 
 
@@ -60,6 +68,74 @@ async def set_starters():
             command="search",
         ),
     ]
+
+
+@cl.step(type="tool", show_input=False)
+async def audio(audio_file):
+    response = await whisper.audio.transcriptions.create(
+        model=os.getenv("GROQ_MODEL"), file=audio_file
+    )
+
+    return response.text
+
+
+async def process_audio():
+    if audio_chunks := cl.user_session.get("audio_chunks"):
+        concatenated = np.concatenate(list(audio_chunks))
+        wav_buffer = io.BytesIO()
+        with wave.open(wav_buffer, "wb") as wav_file:
+            wav_file.setnchannels(1)  # mono
+            wav_file.setsampwidth(2)  # 2 bytes per sample (16-bit)
+            wav_file.setframerate(24000)  # sample rate (24kHz PCM)
+            wav_file.writeframes(concatenated.tobytes())
+
+        wav_buffer.seek(0)
+
+        cl.user_session.set("audio_chunks", [])
+
+    frames = wav_file.getnframes()
+    rate = wav_file.getframerate()
+
+    duration = frames / float(rate)
+    if duration <= 1.71:
+        print("The audio is too short, please try again.")
+        return
+
+    audio_buffer = wav_buffer.getvalue()
+
+
+    whisper_input = ("audio.wav", audio_buffer, "audio/wav")
+    transcription = await audio(whisper_input)
+
+    user_message = cl.Message(
+        content=transcription,
+        author="User",
+        type="user_message",
+    )
+    await user_message.send()
+
+    await on_message(user_message)
+
+
+@cl.on_audio_start
+async def on_audio_start():
+    cl.user_session.set("audio_chunks", [])
+    return True
+
+
+@cl.on_audio_chunk
+async def on_audio_chunk(chunk: cl.InputAudioChunk):
+    audio_chunks = cl.user_session.get("audio_chunks")
+
+    if audio_chunks is not None:
+        audio_chunk = np.frombuffer(chunk.data, dtype=np.int16)
+        audio_chunks.append(audio_chunk)
+
+
+@cl.on_audio_end
+async def on_audio_end():
+    await process_audio()
+    return True
 
 
 @cl.on_chat_start
